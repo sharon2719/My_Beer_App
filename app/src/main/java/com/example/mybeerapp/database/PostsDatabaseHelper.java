615e8b6d86e4048e4a0f4ee1583421c84db7c0a1
sharon2719 <otiennosharon@gmail.com>
2022-04-21 11:28:42 +0300

    beer updates

---
+++ app/src/main/java/com/example/mybeerapp/database/PostsDatabaseHelper.java
@@ -0,0 +1,13 @@
+package com.example.mybeerapp.database;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PostsDatabaseHelper extends SQLiteOpenHelper {
+
+//    Database info
+    private static final String DATABASE_NAME = "beerDatabase";
+    private static  final int DATABASE_VERSION = 1;
+
+//    Table names
+    private static final String
+}
